b3715e78573df0aef2958c1b203d9f6026836e0c
CosmoD0G <acneff27@gmail.com>
2026-03-04 02:04:48 -0500

    created window file

---
+++ AdminWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class AdminWindow extends JFrame {
+    
+    public AdminWindow() {
+        setTitle("Admin Window");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); // Center the window
+    }
+}
